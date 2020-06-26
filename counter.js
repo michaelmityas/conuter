8c32e5879816f40ef6bd91cfa8ab937d4b5b4899
Michael Mityas <62957709+michaelmityas@users.noreply.github.com>
2020-06-26 23:54:26 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -10,21 +10,26 @@ function increment(){
     
     i++;
     countNumber.innerHTML = i; 
-if(i < 0){
+if(i == 0){
+        countNumber.style.color = "white";
+    }else if(i < 0){
         countNumber.style.color = "red";
     }else{
         countNumber.style.color = "rgb(147, 245, 122)";
     }
+    countNumber.animate([{opacity: "0.2"},{opacity: "1.0"}],1000);
 } 
 
 function decrement(){
     
     i--;
     countNumber.innerHTML = i;
-    if(i < 0){
+    if(i == 0){
+        countNumber.style.color = "white";
+    }else if(i < 0){
         countNumber.style.color = "red";
     }else{
         countNumber.style.color = "rgb(147, 245, 122)";
     }
-
+    countNumber.animate([{opacity: "0.2"},{opacity: "1.0"}],1000);
 } 
